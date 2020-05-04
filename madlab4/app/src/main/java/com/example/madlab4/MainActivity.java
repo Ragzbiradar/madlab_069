005e77c3ddbe9fbb65f8ff6e71e1c04db87c8ccf
Ragzbiradar <raghavendragb1999@gmail.com>
2020-05-04 21:05:28 +0530

    updated codes

--- madlab4/app/src/main/java/com/example/madlab4/MainActivity.java
+++ madlab4/app/src/main/java/com/example/madlab4/MainActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class    MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
